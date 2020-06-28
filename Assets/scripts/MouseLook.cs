d33982bd242d01572e22e66747f7cc56fb9bd4a0
Vaibhav Upadhyay <8084054+imvaibhav28@users.noreply.github.com>
2020-06-28 23:03:32 +0530

    Adding game files

---
+++ Assets/scripts/MouseLook.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 200f;
+    public Transform CamAxis;
+
+    public Transform PlayerBodyParent;
+
+    public float playerSpeed = 12f;
+    float xRotation = 0f;
+    // Start is called before the first frame update
+
+    //public Transform PMove;
+
+    void Start()
+    {
+        //cam = Camera.main;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 45f);
+
+
+
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+
+        //float offset = PlayerBodyParent.rotation.y - CamAxis.rotation.y;
+
+        if (Input.GetButton("Vertical")|| Input.GetButton("Horizontal"))
+        {
+            //CamAxis.Rotate(Vector3.up * PlayerBodyParent.transform.rotation.y);
+            CamAxis.rotation = PlayerBodyParent.rotation; ///for now this is working let it be(will work on rotation later)
+            //Vector3 rotangle=new Vector3(0f, offset, 0f);
+            //CamAxis.Rotate(rotangle);
+            PlayerBodyParent.Rotate(Vector3.up * mouseX); //only dependency here for toonsoldier
+            //CamAxis.Rotate(Vector3.right * PlayerBodyParent.position.x);
+
+        }
+
+        else if(Input.GetButton("Vertical") && Input.GetButton("Horizontal"))
+        {
+            CamAxis.Rotate(Vector3.up * mouseX);
+        }
+        else
+        {
+            CamAxis.Rotate(Vector3.up * mouseX);
+        }
+
+        float xCord = Input.GetAxis("Horizontal") * playerSpeed*Time.deltaTime;
+        float zCord = Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
+
+
+
+
+    }
+}
---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+
+    public Rigidbody body;
+    public float turnSpeed = 20f;
+    Animator m_Animator;
+    Vector3 move;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    private void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDir = Input.GetAxis("Horizontal");
+        float yDir = Input.GetAxis("Vertical");
+
+        move = transform.right * xDir + transform.forward * yDir;
+        controller.Move(move * speed * Time.deltaTime);
+
+        //Below Code to start Walking Animation
+        bool hasHorizontalInput = !Mathf.Approximately(xDir, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(yDir, 0f);
+
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        //Vector3 desiredForward = Vector3.zero;
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, move, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+
+
+        //if (Input.GetButton("Horizontal"))
+        //{
+        //    Debug.Log("Horzontal Pressed");
+        //    body.position.Set(controller.);
+               
+        //}
+
+        //if (Input.GetButton("Vertical"))
+        //{
+        //    desiredForward = Vector3.RotateTowards(transform.forward, move, turnSpeed * Time.deltaTime, 0f);
+
+        //}
+        //m_Rotation = Quaternion.LookRotation(desiredForward);
+
+        //    //Vector3 desiredForward = Vector3.RotateTowards(transform.forward, move, turnSpeed * Time.deltaTime, 0f);
+
+        //    body.MovePosition(body.position + (move * m_Animator.deltaPosition.magnitude));
+        //    body.MoveRotation(m_Rotation);
+    }
+
+    void OnAnimatorMove()
+    {
+        Debug.Log("here");
+
+        //body.MovePosition(body.position + (move * m_Animator.deltaPosition.magnitude));
+        body.MoveRotation(m_Rotation);
+
+    }
+}
+
+
+//void FixedUpdate()
+//{
+//    float horizontal = Input.GetAxis("Horizontal");
+//    float vertical = Input.GetAxis("Vertical");
+
+//    m_Movement.Set(horizontal, 0f, vertical);
+//    m_Movement.Normalize();
+
+//    bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+//    bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+//    bool isWalking = hasHorizontalInput || hasVerticalInput;
+//    m_Animator.SetBool("IsWalking", isWalking);
+
+//    Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+//    m_Rotation = Quaternion.LookRotation(desiredForward);
+//}
+
+//void OnAnimatorMove()
+//{
+//    m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+//    m_Rigidbody.MoveRotation(m_Rotation);
+//}
